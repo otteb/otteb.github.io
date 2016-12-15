b364ef5b990e97f17d293c551878e091effa34e6
Brian Otte <brianotte@Brians-MacBook-Pro-2.local>
2016-12-14 20:46:48 -0700

    ripped out gulp... tear tear

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,56 @@
-const gulp = require('gulp');
-//handles employing gulp while being hosted on github
-const ghPages = require('gulp-gh-pages');
-const concat = require('gulp-concat');
-const babel = require('gulp-babel');
-const sass = require('gulp-sass');
+//This Gulp file works offline, but doesn't work when hosted on Github pages.
+//excellent template for future projects
 
-var options = {
-  remoteUrl: "https://otteb.github.io/",
-  branch: "master"
-};
 
-const paths = {
-  jsSource: ['./public/app/app.js', './public/app/**/*.js'],
-  scssSource: './public/**/**/**/*.scss'
-};
 
-gulp.task('js-bundle', () => {
-  gulp.src(paths.jsSource)
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./public/dist'))
-});
 
-gulp.task('scss-bundle', () => {
-  gulp.src(paths.scssSource)
-    .pipe(sass())
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest('./public/dist'))
-});
 
-//pipes everything from the dist folder into ghPages to be deployed
-gulp.task('deploy', function(){
-  return gulp.src('./public/app/dist/**/*')
-    .pipe(ghPages(options));
-});
 
-gulp.task('watch', () => {
-  gulp.watch(paths.jsSource, ['js-bundle'])
-  gulp.watch(paths.scssSource, ['scss-bundle'])
-});
 
-gulp.task('default', ['watch', 'deploy', 'js-bundle', 'scss-bundle']);
+
+
+ // const gulp = require('gulp');
+// //handles employing gulp while being hosted on github
+// const ghPages = require('gulp-gh-pages');
+// const concat = require('gulp-concat');
+// const babel = require('gulp-babel');
+// const sass = require('gulp-sass');
+//
+// var options = {
+//   remoteUrl: "https://otteb.github.io/",
+//   branch: "master"
+// };
+//
+// const paths = {
+//   jsSource: ['./public/app/app.js', './public/app/**/*.js'],
+//   scssSource: './public/**/**/**/*.scss'
+// };
+//
+// gulp.task('js-bundle', () => {
+//   gulp.src(paths.jsSource)
+//     .pipe(babel({
+//       presets: ['es2015']
+//     }))
+//     .pipe(concat('all.js'))
+//     .pipe(gulp.dest('./public/dist'))
+// });
+//
+// gulp.task('scss-bundle', () => {
+//   gulp.src(paths.scssSource)
+//     .pipe(sass())
+//     .pipe(concat('styles.css'))
+//     .pipe(gulp.dest('./public/dist'))
+// });
+//
+// //pipes everything from the dist folder into ghPages to be deployed
+// gulp.task('deploy', function(){
+//   return gulp.src('./public/app/dist/**/*')
+//     .pipe(ghPages(options));
+// });
+//
+// gulp.task('watch', () => {
+//   gulp.watch(paths.jsSource, ['js-bundle'])
+//   gulp.watch(paths.scssSource, ['scss-bundle'])
+// });
+//
+// gulp.task('default', ['watch', 'deploy', 'js-bundle', 'scss-bundle']);
